a3cadb916fae14c84cc4847ce5931ed2d912eaec
rockyluke <rockyluke@offline.net>
2015-06-22 08:49:47 +0200

    Remove trailing whitespace

--- manifests/init.pp
+++ manifests/init.pp
@@ -56,7 +56,7 @@ class supervisor (
   $supervisor_sysconfig    = $supervisor::params::supervisor_sysconfig
 
   if $supervisor_sysconfig_options == undef {
-    $supervisor_sysconfig_options = 
+    $supervisor_sysconfig_options =
     $supervisor::params::supervisor_sysconfig_options
   }
 
