2879728ee653c1a550e18ee261c260a34bbef80a
J.Zimmerman <zimmermj@trimet.org>
2016-02-04 11:00:10 -0800

    Add initial support for CentOS/RHEL 7.

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,13 +14,24 @@ class supervisor::params {
       $supervisor_logrotate         = '/etc/logrotate.d/supervisor'
     }
     /(CentOS|Fedora|RedHat)/ : {
-      $supervisor_package_name      = 'supervisor'
-      $supervisor_service_name      = 'supervisor'
-      $supervisor_conf_dir          = '/etc/supervisor/conf.d'
-      $supervisor_conf_file         = 'supervisord.conf'
-      $supervisor_sysconfig         = '/etc/sysconfig/supervisor'
-      $supervisor_sysconfig_options = ''
-      $supervisor_logrotate         = '/etc/logrotate.d/supervisor'
+
+      if (versioncmp($::operatingsystemmajrelease, '7') >= 0) {
+        $supervisor_package_name      = 'supervisor'
+        $supervisor_service_name      = 'supervisord'
+        $supervisor_conf_dir          = '/etc/supervisord.d'
+        $supervisor_conf_file         = '/etc/supervisord.conf'
+        $supervisor_sysconfig         = '/etc/sysconfig/supervisor'
+        $supervisor_sysconfig_options = ''
+        $supervisor_logrotate         = '/etc/logrotate.d/supervisor'
+      } else {
+        $supervisor_package_name      = 'supervisor'
+        $supervisor_service_name      = 'supervisor'
+        $supervisor_conf_dir          = '/etc/supervisor/conf.d'
+        $supervisor_conf_file         = 'supervisord.conf'
+        $supervisor_sysconfig         = '/etc/sysconfig/supervisor'
+        $supervisor_sysconfig_options = ''
+        $supervisor_logrotate         = '/etc/logrotate.d/supervisor'
+      }
     }
     default                  : {
       fail("Module supervisor is not supported on ${::operatingsystem}")
